90ef6c21b81f586b77c22bb0ce1276bf3575e093
arifu00 <arifu5758@gmail.com>
2023-11-07 01:18:04 +0600

    Implemented search functionality by food name.

--- src/Components/FeaturedFoods.jsx
+++ src/Components/FeaturedFoods.jsx
@@ -11,6 +11,8 @@ import {
 } from "@material-tailwind/react";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { motion } from 'framer-motion';
+
 
 const FeaturedFoods = () => {
   const [foods, setFoods] = useState([]);
@@ -21,6 +23,11 @@ const FeaturedFoods = () => {
       .then((res) => res.json())
       .then((data) => setFoods(data));
   }, []);
+
+  const motionVariants = {
+    hover: { scale: 1 },
+    initial: { scale: 0.9 },
+  };
   return (
     <div className="my-12  md:mt-32 container mx-auto px-2 md:px-5 md:my-20">
       <div className="">
@@ -30,64 +37,73 @@ const FeaturedFoods = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {foods.map((food) => (
-          <Card key={food.id} className=" overflow-hidden shadow-xl ">
-            <CardHeader
-              shadow={true}
-              color="transparent"
-              className="m-0 rounded-none"
-            >
-              <img
-                src={food.foodImage}
-                alt={food.foodName}
-                className="h-72 w-full object-cover"
-              />
-            </CardHeader>
-            <CardBody className="h-[200px]">
-              <Typography variant="h4" className="text-[#D59B2D]">
-                || {food.foodName} ||
-              </Typography>
-              <Typography
-                variant="lead"
-                color="gray"
-                className="mt-3 font-medium"
+          <motion.div
+            key={food.id}
+            variants={motionVariants}
+      initial="initial"
+      whileHover="hover"
+          >
+            <Card className=" overflow-hidden shadow-xl ">
+              <CardHeader
+                shadow={true}
+                color="transparent"
+                className="m-0 rounded-none"
               >
-                <span className="font-bold text-[#6E7C90]">Food Taste:</span>{" "}
-                {food.additionalNotes}
-              </Typography>
-              <Typography variant="lead" color="gray" className="font-medium">
-                <span className="font-bold text-[#6E7C90]">Expired Date:</span>{" "}
-                {food.expiredDate}
-              </Typography>
-              <Typography variant="lead" color="gray" className="font-medium">
-                <span className="font-bold text-[#6E7C90]">
-                  Pickup Location:
-                </span>{" "}
-                {food.pickupLocation}
-              </Typography>
-            </CardBody>
-            <CardFooter className="flex items-center justify-between">
-              <div className="flex items-center ">
-                <Tooltip content={food.donatorName}>
-                  <Avatar
-                    size="lg"
-                    variant="circular"
-                    alt={food.donatorName}
-                    src={food.donatorImage}
-                    className="border-2 border-white hover:z-10 cursor-pointer"
-                  />
-                </Tooltip>
-              </div>
-              <button className="flex items-center gap-3 cursor-pointer font-medium text-xl hover:text-[#D59B2D] bg-amber-300 px-4 py-2 rounded-lg hover:bg-[#494a4b]">
-                <h6>More</h6>
-                <BsArrowRight className="hover:text-[#FF3811]"></BsArrowRight>
-              </button>
-            </CardFooter>
-          </Card>
+                <img
+                  src={food.foodImage}
+                  alt={food.foodName}
+                  className="h-72 w-full object-cover"
+                />
+              </CardHeader>
+              <CardBody className="h-[200px]">
+                <Typography variant="h4" className="text-[#D59B2D]">
+                  || {food.foodName} ||
+                </Typography>
+                <Typography
+                  variant="lead"
+                  color="gray"
+                  className="mt-3 font-medium"
+                >
+                  <span className="font-bold text-[#6E7C90]">Food Taste:</span>{" "}
+                  {food.additionalNotes}
+                </Typography>
+                <Typography variant="lead" color="gray" className="font-medium">
+                  <span className="font-bold text-[#6E7C90]">
+                    Expired Date:
+                  </span>{" "}
+                  {food.expiredDate}
+                </Typography>
+                <Typography variant="lead" color="gray" className="font-medium">
+                  <span className="font-bold text-[#6E7C90]">
+                    Pickup Location:
+                  </span>{" "}
+                  {food.pickupLocation}
+                </Typography>
+              </CardBody>
+              <CardFooter className="flex items-center justify-between">
+                <div className="flex items-center ">
+                  <Tooltip content={food.donatorName}>
+                    <Avatar
+                      size="lg"
+                      variant="circular"
+                      alt={food.donatorName}
+                      src={food.donatorImage}
+                      className="border-2 border-white hover:z-10 cursor-pointer"
+                    />
+                  </Tooltip>
+                </div>
+                <button className="flex items-center gap-3 cursor-pointer font-medium text-xl hover:text-[#D59B2D] bg-amber-300 px-4 py-2 rounded-lg hover:bg-[#494a4b]">
+                  <h6>More</h6>
+                  <BsArrowRight className="hover:text-[#FF3811]"></BsArrowRight>
+                </button>
+              </CardFooter>
+            </Card>{" "}
+          </motion.div>
         ))}
       </div>
       <div className="text-center mt-20">
         <button className="text-lg font-semibold px-4 py-3 rounded-lg text-[#D59B2D] border-2 border-[#D59B2D] hover:text-white hover:bg-[#FF3811] hover:border-0 ">
-         <Link to='/availableFoods'>All Foods</Link> 
+          <Link to="/availableFoods">All Foods</Link>
         </button>
       </div>
     </div>
--- src/Pages/AvailableFoods/AvailableFoods.jsx
+++ src/Pages/AvailableFoods/AvailableFoods.jsx
@@ -16,12 +16,29 @@ import { Link } from "react-router-dom";
 
 const AvailableFoods = () => {
   const [foods, setFoods] = useState([]);
-  console.log(foods);
+  const [searchTerm, setSearchTerm] = useState("");
+  // console.log(searchTerm);
+
+  // console.log(foods);
   useEffect(() => {
     fetch("http://localhost:5000/foods")
       .then((res) => res.json())
       .then((data) => setFoods(data));
   }, []);
+
+  // searchbar manage
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const filteredFoods = foods?.filter((food) => {
+    return food.foodName.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  // console.log(filteredFoods);
+
   return (
     <div className="">
       {/* top search and filter  */}
@@ -29,7 +46,11 @@ const AvailableFoods = () => {
         <div className="container  mx-auto px-2 flex justify-center items-center gap-12">
           {/* search  */}
           <div className="w-72">
-            <Input label="Search Food" icon={<BiSolidSearch></BiSolidSearch>} />
+            <Input
+              onChange={handleSearchChange}
+              label="Search Food"
+              icon={<BiSolidSearch></BiSolidSearch>}
+            />
           </div>
           {/* filter option  */}
           <div className="w-72">
@@ -40,7 +61,8 @@ const AvailableFoods = () => {
                 unmount: { y: 25 },
               }}
             >
-              <Option>Food Expire Date</Option>
+              <Option className="text-black">Food Expire Date</Option>
+              <Option className="text-black">Food Quantity</Option>
             </Select>
           </div>
         </div>
@@ -48,83 +70,94 @@ const AvailableFoods = () => {
         {/* foods  */}
         <div className="container mx-auto px-4 my-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {foods.map((food) => (
-              <Card key={food.id} className=" overflow-hidden shadow-xl ">
-                <CardHeader
-                  shadow={true}
-                  color="transparent"
-                  className="m-0 rounded-none"
-                >
-                  <img
-                    src={food.foodImage}
-                    alt={food.foodName}
-                    className="h-72 w-full object-cover"
-                  />
-                </CardHeader>
-                <CardBody className="h-[250px]">
-                  <Typography variant="h4" className="text-[#D59B2D]">
-                    || {food.foodName} ||
-                  </Typography>
-                  <Typography
-                    variant="lead"
-                    color="gray"
-                    className="mt-3 font-medium"
+            {filteredFoods.length > 0 ? (
+              filteredFoods.map((food) => (
+                <Card key={food.id} className=" overflow-hidden shadow-xl ">
+                  <CardHeader
+                    shadow={true}
+                    color="transparent"
+                    className="m-0 rounded-none"
                   >
-                    <span className="font-bold text-[#6E7C90]">
-                      Food Taste:
-                    </span>{" "}
-                    {food.additionalNotes}
-                  </Typography>
-                  <Typography
-                    variant="lead"
-                    color="gray"
-                    className="font-medium"
-                  >
-                    <span className="font-bold text-[#6E7C90]">
-                      Expired Date:
-                    </span>{" "}
-                    {food.expiredDate}
-                  </Typography>
-                  <Typography
-                    variant="lead"
-                    color="gray"
-                    className="font-medium"
-                  >
-                    <span className="font-bold text-[#6E7C90]">
-                    Food Quantity :
-                    </span>{" "}
-                    <strong>{food.foodQuantity}</strong> person to be served
-                  </Typography>
-                  <Typography
-                    variant="lead"
-                    color="gray"
-                    className="font-medium"
-                  >
-                    <span className="font-bold text-[#6E7C90]">
-                      Pickup Location:
-                    </span>{" "}
-                    {food.pickupLocation}
-                  </Typography>
-                </CardBody>
-                <CardFooter className="flex items-center justify-between">
-                  <div className="flex items-center ">
-                    <Tooltip content={food.donatorName}>
-                      <Avatar
-                        size="lg"
-                        variant="circular"
-                        alt={food.donatorName}
-                        src={food.donatorImage}
-                        className="border-2 border-white hover:z-10 cursor-pointer"
-                      />
-                    </Tooltip>
-                  </div>
-                  <button className=" cursor-pointer font-medium text-xl hover:text-[#D59B2D] bg-amber-300 px-4 py-2 rounded-lg hover:bg-[#494a4b]">
-                    <Link className="flex items-center gap-3" to={`/food/${food._id}`}><h6>More</h6>
-                    <BsArrowRight className="hover:text-[#FF3811]"></BsArrowRight></Link>
-                  </button>
-                </CardFooter>
-              </Card>
-            ))}
+                    <img
+                      src={food.foodImage}
+                      alt={food.foodName}
+                      className="h-72 w-full object-cover"
+                    />
+                  </CardHeader>
+                  <CardBody className="h-[250px]">
+                    <Typography variant="h4" className="text-[#D59B2D]">
+                      || {food.foodName} ||
+                    </Typography>
+                    <Typography
+                      variant="lead"
+                      color="gray"
+                      className="mt-3 font-medium"
+                    >
+                      <span className="font-bold text-[#6E7C90]">
+                        Food Taste:
+                      </span>{" "}
+                      {food.additionalNotes}
+                    </Typography>
+                    <Typography
+                      variant="lead"
+                      color="gray"
+                      className="font-medium"
+                    >
+                      <span className="font-bold text-[#6E7C90]">
+                        Expired Date:
+                      </span>{" "}
+                      {food.expiredDate}
+                    </Typography>
+                    <Typography
+                      variant="lead"
+                      color="gray"
+                      className="font-medium"
+                    >
+                      <span className="font-bold text-[#6E7C90]">
+                        Food Quantity :
+                      </span>{" "}
+                      <strong>{food.foodQuantity}</strong> person to be served
+                    </Typography>
+                    <Typography
+                      variant="lead"
+                      color="gray"
+                      className="font-medium"
+                    >
+                      <span className="font-bold text-[#6E7C90]">
+                        Pickup Location:
+                      </span>{" "}
+                      {food.pickupLocation}
+                    </Typography>
+                  </CardBody>
+                  <CardFooter className="flex items-center justify-between">
+                    <div className="flex items-center ">
+                      <Tooltip content={food.donatorName}>
+                        <Avatar
+                          size="lg"
+                          variant="circular"
+                          alt={food.donatorName}
+                          src={food.donatorImage}
+                          className="border-2 border-white hover:z-10 cursor-pointer"
+                        />
+                      </Tooltip>
+                    </div>
+                    <button className=" cursor-pointer font-medium text-xl hover:text-[#D59B2D] bg-amber-300 px-4 py-2 rounded-lg hover:bg-[#494a4b]">
+                      <Link
+                        className="flex items-center gap-3"
+                        to={`/food/${food._id}`}
+                      >
+                        <h6>More</h6>
+                        <BsArrowRight className="hover:text-[#FF3811]"></BsArrowRight>
+                      </Link>
+                    </button>
+                  </CardFooter>
+                </Card>
+              ))
+            ) : (
+              <div className="text-center col-span-3">
+                <p className=" text-red-500 font-bold ">No Food Match</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
