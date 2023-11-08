520569a9d70a051faf359934444607392ca500f1
arifu00 <arifu5758@gmail.com>
2023-11-08 20:07:46 +0600

    added jwt setup

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,10 +1,35 @@
 import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const instance = axios.create({
   baseURL: "http://localhost:5000/",
   withCredentials: true,
 });
 const useAxios = () => {
+  const { logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (error) => {
+        console.log("error tracked in the interceptors ", error.response);
+        if (error.response.status === 401 || error.response.status === 403) {
+          console.log("Logged Out The user");
+          logOut()
+            .then(() => {
+              navigate("/login");
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }
+    );
+  }, []);
   return instance;
 };
 
